b5c998728e075ca038f2a90b786c9c60e41816c4
PraveenKD <praveenkdewangan90@gmail.com>
2020-06-13 19:23:16 +0530

    first skeleton commit

---
+++ app/src/main/java/com/example/articles/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.articles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.articles.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
